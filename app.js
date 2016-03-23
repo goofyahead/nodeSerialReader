2b5fc7cbf9c618ace14c1f38b05136ba51ee5fed
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2016-03-23 19:50:16 +0100

    find arduino

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort; // localize object constructor
 
-var sp = new SerialPort("/dev/cu.usbmodem1421", {
-	parser: serialport.parsers.readline("\n"),
-	baudrate: 9600
-}, false); 
+serialport.list(function (err, ports) {
+	ports.forEach(function(port) {
+		if (port.manufacturer) {
+			if (port.manufacturer.indexOf('Arduino') > -1) {
+				console.log('Arduino found, connecting.......');
+				var sp = new SerialPort(port.comName, {
+					parser: serialport.parsers.readline("\n"),
+					baudrate: 9600
+				}, false); 
 
-sp.open(function (error) {
-	if ( error ) {
-		console.log('failed to open: '+error);
-	} else {
-		console.log('open');
-		sp.on('data', function(data) {
-			console.log('data received: ' + data);
-		});
-    // serialPort.write("ls\n", function(err, results) {
-    //   console.log('err ' + err);
-    //   console.log('results ' + results);
-    // });
-}
+				sp.open(function (error) {
+					if ( error ) {
+						console.log('failed to open: '+error);
+					} else {
+						console.log('opened connection:');
+						sp.on('data', function(data) {
+							console.log('data received: ' + data);
+						});
+					}
+				});
+			}
+			console.log(port.manufacturer);
+		}
+	});
 });
+
